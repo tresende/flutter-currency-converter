0d80abf4aaa5abdf37f60d9f454e146b415fe8d4
tresende <thiago.gcresende@gmail.com>
2019-03-02 15:09:47 -0300

    Criando interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
 import 'dart:convert';
 
 const request = 'https://api.hgbrasil.com/finance?key=8004b0aa';
@@ -10,6 +9,7 @@ void main() async {
 
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(hintColor: Colors.amber, primaryColor: Colors.white),
   ));
 }
 
@@ -24,6 +24,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +36,58 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         title: Text("\$ Conversor \$"),
       ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapeshot) {
+          switch (snapeshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: Text("Carregando Dados",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.amber, fontSize: 20)));
+            default:
+              if (snapeshot.hasError) {
+                return Center(
+                    child: Text("Erro ao Carregando Dados",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.amber, fontSize: 20)));
+              }
+              dolar = snapeshot.data["results"]["currencies"]["USD"]["buy"];
+              euro = snapeshot.data["results"]["currencies"]["EUR"]["buy"];
+              return SingleChildScrollView(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Icon(
+                      Icons.monetization_on,
+                      size: 150,
+                      color: Colors.amber,
+                    ),
+                    buildTextField("Reais", "R\$"),
+                    Divider(),
+                    buildTextField("Dólares", "\$"),
+                    Divider(),
+                    buildTextField("Euros", "€"),
+                  ],
+                ),
+              );
+          }
+        },
+      ),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix) {
+  return TextField(
+    style: TextStyle(color: Colors.amber, fontSize: 20),
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.amber),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+    ),
+  );
 }
